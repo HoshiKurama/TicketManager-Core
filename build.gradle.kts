e1b05f2b777d60cff3a47764c32b2ecf30efe1f4
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2024-11-04 10:41:48 -0800

    Update dependencies to latest stable versions MC 1.21 w/ Java 21 is new minimum

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.0.21"
+    id("com.gradleup.shadow") version "8.3.5"
     id("com.github.ben-manes.versions") version "0.51.0"
     application
 }
@@ -8,24 +8,24 @@ plugins {
 application.mainClass.set("com.github.hoshikurama.tmcore.Plugin")
 
 group = "com.github.hoshikurama"
-version = "11.1.1"
+version = "11.1.2"
 
 repositories {
     mavenCentral()
-    maven(url = "https://papermc.io/repo/repository/maven-public/")
+    maven(url = "https://repo.papermc.io/repository/maven-public/")
     maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
 
     // Shade these in for Kotlin users
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.6.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.7.3")
 
     implementation("com.github.HoshiKurama.TicketManager_API:Common:11.1.0")
     implementation("com.github.HoshiKurama.TicketManager_API:TMCoroutine:11.1.0")
@@ -36,9 +36,7 @@ tasks {
     shadowJar {
         configurations = listOf(project.configurations.runtimeClasspath.get())
 
-        dependencies {
-            exclude { it.moduleGroup == "net.kyori" }
-        }
+        dependencies {}
 
         relocate("kotlin", "com.github.hoshikurama.ticketmanager.shaded.kotlin")
         relocate("kotlinx", "com.github.hoshikurama.ticketmanager.shaded.kotlinx")
@@ -46,5 +44,5 @@ tasks {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
